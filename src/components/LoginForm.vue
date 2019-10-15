5d00c1e18196040f1ae6702b3fb470dd439c1d3c
catherine-o <catherine.ohara@outlook.com>
2019-10-15 13:14:48 -0600

    adds submit button to login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,6 +4,7 @@
         <input id="username" name="username" type="text" placeholder='Username'>
         <!-- <label for="password">Password</label> -->
         <input id="password" name="password" type="password" placeholder='Password'>
+        <button>Submit</button>
     </form>
 </template>
 
@@ -11,13 +12,21 @@
 .login-form {
     display: flex;
     flex-direction: column;
-    input {
+    input, button {
         width: 300px;
         height: 35px;
         border-radius: 10px;
         margin: 15px auto;
         font-size: 15px;
         padding: 3px 15px;
+        font-family: 'Julius Sans One', sans-serif;
+    }
+    button {
+        box-shadow: 0.5px 4px 6px 1px #b8b8b8;
+    }
+    button:hover {
+        cursor: pointer;
+        box-shadow: 0 0 0 0;
     }
 }
 </style>
