04664eae2e4ed54790441245003397f0cf12f178
catherine-o <catherine.ohara@outlook.com>
2019-10-19 15:48:47 -0600

    renders week or month component based on selection

---
+++ src/components/Week.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Show week</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,62 @@
 <template>
-    <div>
-        <h1>Profile</h1>
+    <div class='profile'>
+        <div class='header'>
+            <h1>Here is your</h1>
+            <form>
+                <select v-model='selected' class='selection' @change='changeSelected()'>
+                    <option value='week'>Week</option>
+                    <option value='month'>Month</option>
+                </select>
+            </form>
+            <h1>at a glance</h1>
+        </div>
+        <div v-if='showWeek'>
+            <Week  />
+        </div>
+        <div v-else-if='showMonth'>
+            <Month  />
+        </div>
     </div>
 </template>
 
-<style lang="scss">
+<script>
+import Week from '../components/Week'
+import Month from '../components/Month'
+export default {
+    data(){
+        return{
+            selected: 'week',
+            showWeek: true,
+            showMonth: false
+        }
+    },
+    components: {
+        Week,
+        Month
+    },
+    methods: {
+        changeSelected(){
+            this.selected === 'week'
+            ? (this.showWeek = true,
+                this.showMonth = false)
+            : this.showWeek = false, 
+                this.showMonth = true
+        }
+    }
+}
+</script>
 
+<style lang="scss">
+.header{
+    padding-left: 10%;
+    display: flex;
+    .selection {
+        font-size: 32px;
+        font-family: 'Titillium Web', sans-serif;
+        border: none;
+        margin: 13px;
+        background-color: #26a7fd13;
+        color: rgb(93, 96, 97);
+    }
+}
 </style>
