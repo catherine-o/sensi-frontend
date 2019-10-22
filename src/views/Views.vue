3dbde6efc9718aa168558564e098496981c88680
catherine-o <catherine.ohara@outlook.com>
2019-10-22 16:11:28 -0600

    adds videos in Views and responsively renders

--- src/views/Views.vue
+++ src/views/Views.vue
@@ -4,13 +4,17 @@
         <ul class='videos'>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/14O7AxqjiVY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/BbT_e8lWWdo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/MSh49ZJCUdU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/qhLExhpXX0E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/7AkbUfZjS5k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/Ek6nZICuAxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="340" height="192" src="https://www.youtube.com/embed/gU9uURLxonI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
-            <!-- <li><iframe width="340" height="192" src="https://www.svrf.com/embed/vr/915193" allowfullscreen></iframe></li> -->
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/aQd41nbQM-U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/FOZ5OAoI4Jo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/a1OoOdTNiUM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/5vdZuv637Xs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="340" height="192" src="https://www.youtube.com/embed/2S3ufI6atDg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
         </ul>
-        <!-- <Pano type='video' source='https://www.youtube.com/watch?list=PLbsGxdAPhjv_tgCNWGaGvSnlIMZNkmnrM'></Pano> -->
     </div>
 </template>
 
@@ -27,15 +31,17 @@ export default {
 }
 .videos {
     display: flex;
-    padding: 0;
+    padding: 10% 5%;
     flex-wrap: wrap;
     list-style: none;
     justify-content: center;
-    margin: auto;
+    margin: 0 auto;
+    @media only screen and (min-width: 1500px){
+        padding: 1%;
+    }
     li {
         width: 340px;
         height: 192px;
-
     }
 }
 </style>
